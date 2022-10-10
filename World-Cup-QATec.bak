42cec34b1a155d903fdcae9a2ac2001fab87e354
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2022-10-09 19:36:01 -0600

    Create random initial population

---
+++ World-Cup-QATec.bak
@@ -0,0 +1,19 @@
+#lang racket
+
+
+(define (rand num cycles)
+  (cond
+    ((zero? cycles)
+     '())
+    (else
+     (cons (list (random num)) (rand num (- cycles 1))))))
+
+(rand 2 10)
+
+;; First Gen
+
+(define (team defenders midFilders forwards goalKeeper)
+  (cond
+    (()
+     )
+    ))
--- World-Cup-QATec.rkt
+++ World-Cup-QATec.rkt
@@ -1,6 +1,5 @@
 #lang racket
 
-
 (define (rand num cycles)
   (cond
     ((zero? cycles)
@@ -12,8 +11,47 @@
 
 ;; First Gen
 
-(define (team defenders midFilders forwards goalKeeper)
-  (cond
-    (()
-     )
+(define (Population teams defenders1 midFilders1 forwards1 defenders2 midFilders2 forwards2  )
+  (cond 
+    ((zero? teams)
+    '())
+    ((equal? teams 2)
+    (cons (Team defenders2 midFilders2 forwards2) (Population (- teams 1) defenders1 midFilders1 forwards1 defenders2 midFilders2 forwards2)))
+    ((equal? teams 1)
+    (cons (Team defenders1 midFilders1 forwards1) (Population (- teams 1) defenders1 midFilders1 forwards1 defenders2 midFilders2 forwards2)))
     ))
+
+(define (Team defenders midFilders forwards )
+  (cond
+    ((and (zero? defenders) (zero? midFilders) (zero? forwards))
+    (list (Player 1 3))
+    )
+    ((> defenders 0)
+    (cons (Player 2 3) (Team (- defenders 1) midFilders forwards))
+    )
+    ((> midFilders 0)
+    (cons (Player 3 3) (Team defenders (- midFilders 1) forwards))
+    )
+    ((> forwards 0)
+    (cons (Player 4 3) (Team  defenders midFilders (- forwards 1)))
+    )
+    ))
+
+
+(define (BinaryGenerator numberBits)
+  (cond 
+  ((zero? numberBits)
+    '())
+  (else
+    (cons (random 2) (BinaryGenerator (- numberBits 1))))))
+
+
+(define (Player playerId cycles)
+  (cond
+  ((zero? cycles)
+  (list playerId))
+  (else
+  (cons (BinaryGenerator 4) (Player playerId (- cycles 1)))))
+  )
+
+(Population 2 4 4 2 5 3 2)
