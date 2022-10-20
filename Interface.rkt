a4174fcc7bb0855a962d3d4bee052e6d7570a7cd
Diana Mejías Hernández <85594834+dmejias08@users.noreply.github.com>
2022-10-19 18:21:32 -0600

    add documentation

--- Interface.rkt
+++ Interface.rkt
@@ -11,16 +11,18 @@
 
 ((draw-pixmap mainWindow) "assets/futcourt.png" (make-posn 0 0))
 
-; FUNCION PARA DIBUJAR JUGADORES
+; Function that draw players on screen
 (define (Jugadores list color num)
   (cond ((>= (length list) 1)
              ((draw-solid-ellipse mainWindow) (make-posn (caar list) (last(car list))) 20 20 color)
              (Jugadores (cdr list) color (+ num 1))
              ((draw-string mainWindow) (make-posn (+ (caar list) 3) (+ (last(car list)) 15)) (number->string num)))))
 
+; Function that draw ball on canvas
 (define (Ball position)
   ((draw-solid-ellipse mainWindow) (make-posn (car position) (last position)) 20 20 "white"))
 
+; Refresh function paints canvas in every iteration
 (define (Refresh team1 team2 ball marker)
   ((draw-pixmap mainWindow) "assets/futcourt.png" (make-posn 0 0))
   ((draw-solid-rectangle mainWindow) (make-posn 555 0) 95 35 "white")
@@ -29,10 +31,10 @@
   (Jugadores team1 "blue" 1)
   (Jugadores team2 "red" 12)
   (Ball ball)
-  
   (copy-viewport mainWindow mainFrame)
   ((clear-viewport mainWindow)))
 
+; Auxiliar function
 (define (len lista)
   (cond(
         (null? lista)
@@ -40,6 +42,8 @@
        (else
         (+ 1 (len (cdr lista))))))
 
+
+;; It converts binary to an interget 
 (define (BinaryToInterger binary lenOfNumber)
   (cond
     ((<= lenOfNumber 0)
@@ -47,41 +51,48 @@
     (else
      ( + ( * (car binary) (expt 2 (- lenOfNumber 1))) (BinaryToInterger (cdr binary) (- lenOfNumber 1))))))
 
+; It modify coordenates in a certain index of list 
 (define (ModifyList list index pair)
   (ModifyListAux list 1 index pair))
 
+; Auxiliar function
 (define (ModifyListAux list currentIndex finalIndex pair)
   (cond ((equal? currentIndex finalIndex)
         (cons pair (cdr list)))
         (else
          (cons (car list) (ModifyListAux (cdr list) (+ currentIndex 1) finalIndex pair ) ))))
 
+; It returns true false is an individual is not moving 
 (define (Moving? team1 team2)
         (cond
           ((and (null? team1) (null? team2))
            #f)
-          ((and (< (abs (- (caar team1) (caar team2))) 10)  (< (abs (- (cadar team1) (cadar team2) 10)))
+          ((and (< (abs (- (caar team1) (caar team2))) 5)  (< (abs (- (cadar team1) (cadar team2) 5)))
            (Moving? (cdr team1) (cdr team2))))
           (else
            #t)))
 
+; It takes a list of positions and an position and determines if position is in positions
 (define (Collisions positions pair)
   (cond ((null? positions)
-         #t)
-        ((and (< (abs (- (caar positions)(car pair))) 10 ) (< (abs (- (cadar positions)(cadr pair))) 10 ))
          #f)
+        ((and (< (abs (- (caar positions)(car pair))) 5 ) (< (abs (- (cadar positions)(cadr pair))) 5 ))
+         #t)
         (else
          (Collisions (cdr positions) pair))))
 
+; It takes a position and returns a list that will has the position 11 times
 (define (FollowBall pair)
   (FollowBallAux pair '() 1))
 
+; Auxiliar function for follow ball
 (define (FollowBallAux pair list counter)
   (cond ((equal? counter 12)
          list)
         (else
          (FollowBallAux pair (cons pair list)(+ counter 1)))))
 
+; Get player ids in a list 
 (define (GetPlayersIds population teamNumber)
   (cond
     ((equal? teamNumber 1)
@@ -89,6 +100,7 @@
     ((equal? teamNumber 2)
     (GetPlayersIdsAux (cadr  population) '()))))
 
+; Auxiliar of get player ids
 (define (GetPlayersIdsAux team result)
   (cond
     ((equal? (len team) 1)
@@ -96,6 +108,7 @@
     (else
      (GetPlayersIdsAux (cdr team) (cons (last (car team)) result)))))
 
+; get an specific aattribute of all players in each one team
 (define (GetAttributes population team attribute)
   (cond
     ((equal? team 1)
@@ -103,6 +116,7 @@
     (else
      (GetAttributesAux (cadr population) attribute))))
 
+; Auxiliar of get attributes
 (define (GetAttributesAux team attribute)
   (cond
     ((null? team)
@@ -114,6 +128,7 @@
     ((equal? attribute "ability")
      (cons (GetPlayerAttribute (car team) 3 1) (GetAttributesAux (cdr team) attribute)))))
 
+; Auxuliar of get attributes 
 (define (GetPlayerAttribute player attributePos cont)
   (cond
     ((> cont 3)
@@ -123,6 +138,7 @@
     (else
      (GetPlayerAttribute (cdr player) attributePos (+ cont 1)))))
 
+; Generate position of all players to allign them randomly
 (define (GeneratePositions defenders1 midFielders1 forwards1 defenders2 midFielders2 forwards2)
   (cond
     ((and ( > (+ defenders1  midFielders1  forwards1) 11) ( > (+ defenders2 midFielders2 forwards2) 11))
@@ -134,6 +150,7 @@
     (else
      (cons (cons (list 70 340) (GeneratePosAuxTeam1 defenders1 midFielders1 forwards1)) (GeneratePositions 0 0 0  defenders2 midFielders2 forwards2) ))))
 
+; Auxiliar of generate positions 
 (define (GeneratePosAuxTeam1 defenders1 midFielders1 forwards1)
   (cond
     ((and (zero? defenders1) (zero? midFielders1) (zero? forwards1))
@@ -146,6 +163,7 @@
      (append (GeneratePosAuxForwards 500 forwards1 0) (GeneratePosAuxTeam1 defenders1 midFielders1 0)))
   ))
 
+; Auxiliar of generate positions
 (define (GeneratePosAuxTeam2 defenders2 midFielders2 forwards2)
   (cond
     ((and (zero? defenders2) (zero? midFielders2) (zero? forwards2))
@@ -157,7 +175,8 @@
     ((not (zero? forwards2))
      (append (GeneratePosAuxForwards 675 forwards2 0) (GeneratePosAuxTeam2 defenders2 midFielders2 0)))
   ))
-    
+
+; Auxiliar of generate positions
 (define (GeneratePosAuxDef1Mid2 initialPosition player counter)
   (cond
     ((equal? player counter)
@@ -167,6 +186,7 @@
     (else
      (cons (list (+ initialPosition 50) ( + 150 ( * (/ 500 player) counter))) (GeneratePosAuxDef1Mid2 initialPosition player (+ counter 1))))))
 
+; Auxiliar of generate positions
 (define (GeneratePosAuxDef2Mid1 initialPosition player counter)
   (cond
     ((equal? player counter)
@@ -176,6 +196,7 @@
     (else
      (cons (list (- initialPosition 50) (+ 150 (* (/ 500 player) counter))) (GeneratePosAuxDef2Mid1 initialPosition player (+ counter 1))))))
 
+; Auxiliar of generate positions
 (define (GeneratePosAuxForwards initialPosition player counter)
   (cond
     ((> counter player)
@@ -183,6 +204,7 @@
     (else
      (cons (list initialPosition (+ 275 (* (/ 200 player) counter))) (GeneratePosAuxDef2Mid1 initialPosition player (+ counter 1))))))
 
+; Auxiliar of calling factor 
 (define (CallMovingFactorAux x1 y1 x2 y2 v h ID type team delta_x delta_y)
   (cond ((or (> (-(abs (- x2 x1))5) 5) (> (-(abs (- y2 y1))5) 5))
          (cond ((and (< x2 1200) (> x2 0) (< y2 700) (> y2 0))
@@ -202,6 +224,7 @@
         )
   )
 
+; It defines the limits of each player, and it does not allow to move forward
 (define (CallMovingFactor x1 y1 x2 y2 v h ID type team)
   (cond ((or(equal? type "x"))
          (CallMovingFactorAux x1 y1 (- x2 (* 3 (MovingFactor x1 y1 x2 y2 v h))) y2 v h ID type team (MovingFactor x1 y1 x2 y2 v h) 0))
@@ -210,6 +233,7 @@
          ))
   )
 
+; Auxiliar of moving factot
 (define (DeltaXY x1 y1 x2 y2 v h ID type team delta_x delta_y)
   (cond ((or(equal? type "x"))
          delta_x)
@@ -218,10 +242,12 @@
          ))
   )
 
+; Auxiliar of moving factor 
 (define (MovingFactor x1 y1 x2 y2 v h)
   (* 0.25 (sqrt (+ (expt (+ v 5) 2) (expt (+ h 5) 2))) (/ (- x2 x1) (sqrt(+ (expt (- x2 x1) 2) (expt (- y2 y1) 2)))))
   )
 
+; Starting movement
 (define (ModifyPosition static_list changing_list list_compare counter velocity ability ID team)
   (cond ((not(null? static_list))
          (ModifyList static_list counter (list (- (caar list_compare) (CallMovingFactor
@@ -251,7 +277,8 @@
         (else
          '()
          )))
-  
+
+; Update positions of each list
 (define (UpdatePositions
          static_list1
          list_compare1
@@ -283,12 +310,13 @@
    ID2
    1
    1
-   (car(ball_pos static_list1 static_list2 ball_position 10 10 marker hasit))
+   (ball_pos static_list1 static_list2 ball_position 10 10 marker hasit)
    marker
    (cadr(ball_pos static_list1 static_list2 ball_position 10 10 marker hasit))
    )
   )
 
+; Auxiliar of define positions
 (define (UpdatePositionsAux
          static_list1
          changing_list1
@@ -331,10 +359,13 @@
          (list(list static_list2 static_list1) (list ball_position hasit))))
   )
 
+
+; Starting position, call generate positions to allign players
 (define (starting_pos defenders1 midFielders1 forwards1 defenders2 midFielders2 forwards2)
   (GeneratePositions defenders1 midFielders1 forwards1 defenders2 midFielders2 forwards2)
   )
 
+; returns which team has the ball
 (define (WhoHasTheBall? team1 team2 last_pos)
   (cond ((not(Collisions team1 last_pos))
          "b"
@@ -344,46 +375,46 @@
          )
         ))
 
-
+; ball positions determines if there is colision and change direction of the ball
 (define (ball_pos team1 team2 last_pos f h marker hasit)
-(cond((and (< (car last_pos) 1200) (> (car last_pos) 0) (< (cadr last_pos) 700) (> (cadr last_pos) 0))
-  (cond
-    ((and (< (car last_pos) 1200) (> (car last_pos) 1150) (< (cadr last_pos) 415) (> (cadr last_pos) 260))
-     (list '(590 340) hasit)
-     )
-    ((and (< (car last_pos) 25) (> (car last_pos) 0) (< (cadr last_pos) 415) (> (cadr last_pos) 260))
-     (list '(590 340) hasit)
-     )
-    (else
-     (cond ((and(Collisions team1 last_pos) (Collisions team2 last_pos))
-            (cond ((equal? hasit "b")
-                   (list(ContinuosMovement last_pos f h 1150 340) hasit)
-                   )
-                  (else
-                   (list (ContinuosMovement last_pos f h 50 340) hasit)))    
-            )
-           (else
-            (cond ((equal? hasit "b")
-                   (list (ContinuosMovement last_pos f h (+ 100(random 1000)) (+ 100(random 500))) "r")
-                   )
-                  (else
-                   (list (ContinuosMovement last_pos f h (+ 100(random 1000)) (+ 100(random 500))) "b")))
- 
-            ))
-     )))(else
-         (list '(590 340) hasit)
-         )))
+  (display hasit)
+  (cond ((and(Collisions team1 last_pos) (Collisions team2 last_pos))
+         (cond ((equal? hasit "r")
+                (ContinuosMovement team1 last_pos f h 1150 340)
+                )
+               (else
+                (ContinuosMovement team2 last_pos f h 50 340)))
          
+         )
+        ((and (< (car last_pos) 1200) (> (car last_pos) 1150) (< (cadr last_pos) 415) (> (cadr last_pos) 260))
+         '(590 340)
+         )
+        ((and (< (car last_pos) 25) (> (car last_pos) 0) (< (cadr last_pos) 415) (> (cadr last_pos) 260))
+         '(590 340)
+         )
+        (else
+         (cond ((equal? (WhoHasTheBall? team1 team2 last_pos) "b")
+                (list last_pos "b")
+                )
+               (else
+                (list last_pos "r")))
+         
+         )
+  ))
 
-(define (ContinuosMovement last_pos f h x y)
-  (list (+ (car last_pos) (* 0.2(MovingFactor x y (car last_pos) (cadr last_pos) f h)))
-               (- (cadr last_pos) (* 0.2(MovingFactor y x (cadr last_pos) (car last_pos) f h))))
+; Continuos movement keeps the ball movings according to its direction
+(define (ContinuosMovement team last_pos f h x y)
+  (list (- (car last_pos) (MovingFactor x y (car last_pos) (cadr last_pos) f h))
+               (- (cadr last_pos) (MovingFactor y x (cadr last_pos) (car last_pos) f h)))
   )
 
+
+; Random move move players to a random position
 (define (RandomMove last_pos random_x random_y f h)
   (list (- (car last_pos) (MovingFactor random_x random_y (car last_pos) (cadr last_pos) 1 1))
         (- (cadr last_pos) (MovingFactor random_y random_x (cadr last_pos) (car last_pos) 1 1))))
 
+; This calls refresh fucntion and creates de animation on canvas 
 (define (RepaintAll positions ball_position marker)
   (Refresh
    (car positions)
@@ -392,6 +423,7 @@
    marker)
    )
 
+; This function gives a new list of new positions to repaint canvas and create animation
 (define (CallToUpdate positions ball_position population marker hasit)
   (UpdatePositions
            (FollowBall ball_position)
@@ -410,6 +442,7 @@
            )
   )
 
+; Auxiliar of active moving
 (define (ActiveMovingAux positions population marker)
   (RepaintAll  (car positions) (caadr positions) marker)
   (cond((not(equal? (car positions) (car(CallToUpdate (car positions) (caadr positions) population marker (cddr positions)))))
@@ -420,11 +453,12 @@
         ))
   )
 
+; This function is responsable of all movement of players, takes info form CallToUpdate
 (define (ActiveMoving positions population marker)
   (ActiveMovingAux positions population marker)
   )
 
-(ActiveMoving (list(starting_pos 4 4 2 4 4 2) '((590 340) "r")) '(
+(ActiveMoving (list(starting_pos 4 4 2 4 4 2) '((590 340) "b")) '(
   (((0 1 1 1) (1 0 1 1) (1 1 0 1) 2)
    ((0 1 1 0) (0 1 0 1) (0 0 1 1) 2)
    ((0 0 1 1) (0 1 0 1) (1 0 1 0) 2)
@@ -448,7 +482,7 @@
    ((0 0 0 0) (0 0 1 1) (0 0 0 0) 4)
    ((0 0 0 1) (0 0 0 1) (0 0 1 1) 1))) '(0 0))
   
-;;(Collisions '((250 35)(368 1150)) '(600 30))
+
 ;;Meter en generation2
 
 #|(CallToUpdate(CallToUpdate (UpdatePositions
@@ -469,9 +503,7 @@
             (ModifyPosition static_list2 list_compare2 counter2 velocity2 ability2)
             (list 590 340)
             (list 0 0))
-
            (sleep/yield 1)
-
 (sleep/yield 0.1)
 (Refresh (list (list 75 340) (list 150 600) (list 200 450) (list 200 250) (list 150 100) (list 300 600) (list 350 450) (list 350 250) (list 300 100) (list 500 500) (list 500 200)) (list (list 1100 340) (list 900 340)) (list 590 340) (list 1 0))
 (sleep/yield 0.1)
